abfd40eb982ec8225e25098e9d57bc341d6717a4
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 12:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function firstChar(text) {
   // your code here
-	if(text.charAt(0)===" "||text==="") return "";
-	return text.charAt(0);
+	if(text==="") return "";
+	for(let i=0;i<text.length;){
+		if(text.charAt(i)===" "){
+			i++;
+		}
+		else{ 
+			return text.charAt(i);
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
-
