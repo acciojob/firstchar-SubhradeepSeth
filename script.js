17c5c63d2104631b717375ed4882049e093a21f8
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 11:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstChar(text) {
   // your code here
+	if(text.charAt(0)===" "||text==="") return "";
+	return text.chatrAt(0);
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
